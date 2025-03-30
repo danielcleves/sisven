5450e1092e17198aa62ac34307faec842c6dbd9a
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-03-30 18:24:10 -0500

    🎉 init: Se crea el proyecto y se configuran las migraciones

---
+++ database/migrations/2025_03_30_183439_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('document_number', 15)->unique();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('addres', 80);
+            $table->date('birthday');
+            $table->string('phone_number', 16);
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
