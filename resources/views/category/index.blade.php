e4a4709c0240d92a602a6c1570bea3578ed5bb1a
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-01 23:01:54 -0500

    ✨ feat: Crea la vista para listar categorías en index.blade.php

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Listado de categorías</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Listado de categorías</h1>
+
+        <a href="{{ route('categories.create') }}" class="btn btn-success">Add</a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                <tr>
+                    <th scope="row">{{ $category->cate_codi }}</th>
+                    <td>{{ $category->cate_nomb }}</td>
+                    <td>{{ $category->cate_desc }}</td>
+                    <td>
+                        <a href="{{ route('categories.edit', ['category'=>$category->cate_codi]) }}" class="btn btn-info">Edit</a>
+                        <form action="{{ route('categories.destroy', ['category' => $category->cate_codi]) }}" method="post" style="display: inline-block">
+                            @method('delete')
+                            @csrf
+                            <input class="btn btn-danger" type="submit" value="Delete">
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Boostrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
